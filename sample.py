cf9a06c7668b81b7f517e35ba4119fa29c3f254e
Demo <danishkaybee@gmail.com>
2025-02-12 14:56:09 +0530

    Changes in sample

--- sample.py
+++ sample.py
@@ -1,2 +1,12 @@
 # This is my new sample python file
 print("Hello World")
+#This is new command line
+print("Hi FeatureDev")
+a = "Ravi chari"
+print("Hello")
+# I am making this changes in the file to test in git
+def plus(a,b):
+    c = a + b
+    return c
+
+print(plus(12,12))
