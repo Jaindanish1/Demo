9e7b269be650da77db0c08aca48082110e00c5b7
Jaindanish1 <danishkaybee@gmail.com>
2025-02-12 15:21:02 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,7 +3,8 @@ print("Hello World")
 #This is new command line
 print("Hi FeatureDev")
 a = "Ravi chari"
-print("Hello")
+print("Hello"
+     git changes done here)
 # I am making this changes in the file to test in git
 #adding in between
 def plus(a,b):
